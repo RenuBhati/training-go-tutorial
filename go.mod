82081a73306524cf60b8caf623023f9c10c30565
Renu Bhati <renurahulbhati@gmail.com>
2025-04-04 16:38:35 +0530

    task 1219 tranfered maingo code to basic , and imported the basics in main.go file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RenuBhati/training-go-tutorial
+
+go 1.23.3
--- main.go
+++ main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "github.com/RenuBhati/training-go-tutorial/basics"
 
 func main() {
-	fmt.Println("Hello, World")
-
-	var name string = "Renu Bhati"
-	var age int = 25
-	fmt.Printf("Name: %s\nAge: %d\n", name, age)
-
-	batsman := "David"
-	runs := 34
-	fmt.Printf("Batsmen: %s\nRuns: %d\n", batsman, runs)
-
-	if runs >= 50 {
-		fmt.Println("Half-century!")
-	} else {
-		fmt.Println("Not a half-century!")
-
-	}
-
-	var wg sync.WaitGroup
-
-	for i := 0; i < 50; i++ {
-		wg.Add(1)
-		go printNumbers(i, &wg)
-	}
-	wg.Wait()
-	fmt.Println(add(5, 6))
-	a := Person{"Renu Bhati", 25}
-	fmt.Printf("Name: %s\nAge: %d\n", a.Name, a.Age)
-}
-func add(a int, b int) int {
-	return a + b
-}
-
-type Person struct {
-	Name string
-	Age  int
-}
-
-func printNumbers(n int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(1 * time.Second)
-	fmt.Println(n)
+	basics.Run_tutorial()
 }
