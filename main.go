31147555f80e9cf505d3212c4aaad03a22af1927
Renu Bhati <renurahulbhati@gmail.com>
2025-04-04 15:20:14 +0530

    task 1219 variables, loops, functions

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Hello, World")
+
+	var name string = "Renu Bhati"
+	var age int = 25
+	fmt.Printf("Name: %s\nAge: %d\n", name, age)
+
+	batsman := "David"
+	runs := 34
+	fmt.Printf("Batsmen: %s\nRuns: %d\n", batsman, runs)
+
+	if runs >= 50 {
+		fmt.Println("Half-century!")
+	} else {
+		fmt.Println("Not a half-century!")
+
+	}
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println(add(5, 6))
 }
+func add(a int, b int) int {
+	return a + b
+}
+
