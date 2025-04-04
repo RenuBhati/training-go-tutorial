fb860bf10ae93695dff75e693702b9664cba0554
Renu Bhati <renurahulbhati@gmail.com>
2025-04-04 22:15:33 +0530

    task 1219 view_user

--- routes/routes.go
+++ routes/routes.go
@@ -12,5 +12,6 @@ func SetupRoutes(app *fiber.App) {
 	fmt.Println("Routes")
 	app.Get("/", controllers.GetHome)
 	app.Get("/users", controllers.GetUsers)
+	app.Get("/api/users", controllers.GetUsersJSON)  
 
 }
